feec217c20afbcd6d9050970549d90ca9b71f133
Jesse Bartola <jrbartola@gmail.com>
2016-12-23 16:04:50 -0500

    Tab bar controller

---
+++ Jungle/EventsViewController.swift
@@ -0,0 +1,28 @@
+//
+//  EventsViewController.swift
+//  Jungle
+//
+//  Created by Jesse Bartola on 12/23/16.
+//  Copyright © 2016 Jungle. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class EventsViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        
+        super.viewDidLoad()
+        
+        
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+}
+
---
+++ Jungle/GroupsViewController.swift
@@ -0,0 +1,28 @@
+//
+//  GroupsViewController.swift
+//  Jungle
+//
+//  Created by Jesse Bartola on 12/23/16.
+//  Copyright © 2016 Jungle. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class GroupsViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        
+        super.viewDidLoad()
+        
+        
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+}
+
