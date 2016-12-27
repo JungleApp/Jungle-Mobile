79df726f9a4a3280713340c4a7dfd9e07f93a0b3
Jesse Bartola <jrbartola@gmail.com>
2016-12-26 21:03:16 -0500

    Fixed table view delegate conformance

---
+++ HomeTableView.swift
@@ -0,0 +1,21 @@
+//
+//  HomeTableView.swift
+//  Jungle
+//
+//  Created by Jesse Bartola on 12/26/16.
+//  Copyright © 2016 Jungle. All rights reserved.
+//
+
+import UIKit
+
+class HomeTableView: UITableView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
---
+++ Jungle/HomeTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  HomeTableViewCell.swift
+//  Jungle
+//
+//  Created by Jesse Bartola on 12/26/16.
+//  Copyright © 2016 Jungle. All rights reserved.
+//
+
+import UIKit
+
+class HomeTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
