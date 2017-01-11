33b0d31d081d31aae527c02d5509e7368dcdd614
Jesse Bartola <jrbartola@gmail.com>
2017-01-11 01:19:22 -0500

    Organization table view cell

---
+++ Jungle/OrganizationsTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  OrganizationsTableViewCell.swift
+//  Jungle
+//
+//  Created by Jesse Bartola on 1/11/17.
+//  Copyright © 2017 Jungle. All rights reserved.
+//
+
+import UIKit
+
+class OrganizationsTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
